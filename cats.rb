64e3a9c3153e32b354754e94598d97af4511d966
rochellesagh <rsrochelli@gmail.com>
2018-08-10 19:46:35 +0000

    Done.

--- cats.rb
+++ cats.rb
@@ -1,7 +1,52 @@
 class Cats
-  def initialize(cute,fuzzy,mean)
-    @mean = mean
-    @fuzzy = fuzzy
+  def initialize(cute,fuzzy,mean,cuddly)
     @cute = cute
+    @fuzzy = fuzzy
+    @mean = mean
+    @cuddly = cuddly
   end 
+  
+  def is_it_cute
+    @cute 
+  end 
+  
+  
+  def is_it_fuzzy
+    @fuzzy 
+  end
+  
+  
+  def is_it_mean
+    @mean
+  end 
+  
+  
+  def is_it_cuddly
+    @cuddly
+  end
+  
+
 end
+
+tiger = Cats.new("Oh, it's super snazzy", "pretty fuzzy", "super duper mean", "absolutely not")
+puts " "
+puts "Is tiger cute?"
+puts tiger.is_it_cute
+puts " "
+puts "Is tiger fuzzy?"
+puts tiger.is_it_fuzzy
+puts " "
+puts "Is tiger mean?"
+puts tiger.is_it_mean
+puts " "
+puts "Is tiger cuddly?"
+puts tiger.is_it_cuddly
+puts "  "
+
+garfield = Cats.new("Ewwwww", "yeah...kinda", "well... he kinda hates mondays", "nope")
+
+puts garfield.is_it_cute
+puts "Is Garfield fuzzy?"
+puts garfield.is_it_fuzzy
+puts garfield.is_it_mean
+puts garfield.is_it_cuddly

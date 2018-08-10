9312182f14490a66cf6e2189ee6cd6ca77a3cbb2
rochellesagh <rsrochelli@gmail.com>
2018-08-10 20:23:02 +0000

    automatically backed up by learn

--- table_group.rb
+++ table_group.rb
@@ -1,1 +1,61 @@
-class Table_group 
+class Table
+  def initialize(tv_show,food,amt_sibs)
+    
+    # @ sign means every instance use these variables 
+    
+    @tv_show = tv show
+    @food = food
+    @amt_sibs = amount of siblings
+
+  end 
+  
+  # following @ trait variables are different for each person, specify in string 
+  
+  
+  def favorite_tv_show
+    @tv_show
+  end 
+  
+  
+  def favorite_food
+    @food
+  end
+  
+  
+  def num_amt_sibs
+    @amt_sibs
+  end 
+  
+  
+  #define method so can call it later 
+  
+  def base_traits 
+    puts "I am a girl. I need oxygen. I love food. I am a part of Kode with Klossy."
+  end 
+  #base traits are same for both Cats.new , but must put them to show 
+
+end
+
+rochelle = Table.new("Game of Thrones (obsessed)", "Sushi", "3")
+
+rochelle.base_traits
+puts " "
+puts " "
+puts "What is Rochelle's favorite TV show?"
+puts rochelle.favorite_tv_show
+puts " "
+puts "What is Rochelle's favorite food?"
+puts rochelle.favorite_food
+puts " "
+puts "How many siblings does Rochelle have?"
+puts rochelle.num_amt_sibs
+puts " "
+puts "  "
+
+gabby = Table.new("Ewwwww", "yeah...kinda", "well... he kinda hates mondays", "nope")
+
+garfield.base_traits
+
+
+
+
